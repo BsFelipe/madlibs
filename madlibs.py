cc5a447e4a098870b8a42dad6c494459ee6f96ba
FelipeBs <felipe.bsilvaa@gmail.com>
2022-04-12 11:45:42 -0300

    madlib basic program

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+# a few ways to concatenate a string
+# food = "pizza"
+# print("I like eat " + food)
+# print("I like eat {}".format(food))
+# print(f"I like eat {food}")
+adj = input("Adjective: ")
+verb1 = input("Verb 1: ")
+verb2 = input("Verb2: ")
+geekperson = input("Geek Person: ")
+
+madlib = f"The Lord of the Rings is so {adj}. Tolkien created the most amazing fantasy world, \
+i love to {verb1}. Keep reading and {verb2} like you are {geekperson}"
+
+print(madlib)
